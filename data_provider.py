44e4ac9ff1a11247a53e362f354c8ba12d5dd2f8
Dawid <szarekdawid@gmail.com>
2019-05-15 16:12:08 +0200

    Dostosowanie stylu kodu

--- data_provider.py
+++ data_provider.py
@@ -20,25 +20,26 @@ class DataProvider:
         if self._list_of_videos is None:
             self._list_of_videos = []
             for i in ZipFile(self._zip_file_path).namelist():
-                if i.startswith(self._annotations_zip) and i.endswith('csv'):   #szukanie arkuszy
-                    for j in range(len(i)-4,3,-1):
+                if i.startswith(self._annotations_zip) and i.endswith('csv'):  # szukanie arkuszy
+                    k = 0
+                    for j in range(len(i) - 4, 3, -1):
                         if i[j] == '/':
-                            k=j+1
+                            k = j + 1
                             break
-                    self._list_of_videos.append(i[k:-4])   # branie tylko nazwy arkusza bez lokalizacji i typu
+                    self._list_of_videos.append(i[k:-4])  # branie tylko nazwy arkusza bez lokalizacji i typu
         return self._list_of_videos
 
     def get_video(self, video_name: str) -> pd.DataFrame:
 
         # sprawdza, czy jest taki film - assert = raise error
-        assert video_name in self.list_of_videos,\
+        assert video_name in self.list_of_videos, \
             f"Video named '{video_name}' wasn't found in zip file '{self._zip_file_path.name}'"
 
-        zip = ZipFile(self._zip_file_path)
+        data_zip = ZipFile(self._zip_file_path)
 
         # stworzenie tablicy - odczytanie csv, nazywanie kolumn etc
         video_data = pd.read_csv(
-            StringIO(str(zip.open(f"{self._annotations_zip}{video_name}.csv").read(), 'utf-8')),
+            StringIO(str(data_zip.open(f"{self._annotations_zip}{video_name}.csv").read(), 'utf-8')),
             header=None,
             names='file_name right_hand_x right_hand_y left_hand_x left_hand_y ball_1_x'
                   ' ball_1_y ball_2_x ball_2_y ball_3_x ball_3_y'.split()
@@ -46,9 +47,8 @@ class DataProvider:
 
         video_data['photo'] = None
         for i, photo_name in video_data['file_name'].iteritems():
-            video_data.loc[i, 'photo'] = Image.open(BytesIO(zip.open(self._videos_zip + photo_name).read()))
+            video_data.loc[i, 'photo'] = Image.open(BytesIO(data_zip.open(self._videos_zip + photo_name).read()))
         return video_data
-
 
     # możliwe, że jest to jednak zbędne, ale zostawiam na razie
     def data_type(self, video_name):
@@ -59,46 +59,36 @@ class DataProvider:
 
         video_name = video_name + '.csv'
         for i in range(len(read_testvideos)):
-            read_testvideos[i] = str(read_testvideos[i],'utf-8')
+            read_testvideos[i] = str(read_testvideos[i], 'utf-8')
 
         for i in range(len(read_trainvideos)):
             read_trainvideos[i] = str(read_trainvideos[i], 'utf-8')
         for i in range(len(read_validationvideos)):
-            read_validationvideos[i] = str(read_validationvideos[i],'utf-8')
-
+            read_validationvideos[i] = str(read_validationvideos[i], 'utf-8')
 
         if video_name in read_testvideos:
-            self.type = 'test'
+            self._type = 'test'
         elif video_name in read_trainvideos:
-            self.type = 'train'
+            self._type = 'train'
         elif video_name in read_validationvideos:
-            self.type = 'validation'
+            self._type = 'validation'
 
-        return(self.type)
+        return self._type
 
-    def get_types(self,type) -> pd.DataFrame:
-        zip = ZipFile(self._zip_file_path)
+    def get_types(self, stage) -> pd.DataFrame:
+        data_zip = ZipFile(self._zip_file_path)
 
-        read_files = zip.open(f'data/{type}videos').read().split()
+        read_files = data_zip.open(f'data/{stage}videos').read().split()
         video_data = []
         for i in range(len(read_files)):
             video_data.append(self.get_video(str(read_files[i], 'utf-8')[0:-4]))
-        pd_data = pd.concat(video_data,ignore_index=True)
+        pd_data = pd.concat(video_data, ignore_index=True)
         return pd_data
-
-
-
-
-
-
 
     # def mix(self, video_name):
     #     video_data = self.get_video(video_name)
     #     video_data["type"] = self.data_type(video_name)
     #     return(video_data)
-
-
-
 
 # if __name__ == '__main__':
 #     DataProvider('F:/Studia/balls-and-hands-in-videos-of-juggling/ballsandhands.zip').get_video('1-apple-red-room-door')
